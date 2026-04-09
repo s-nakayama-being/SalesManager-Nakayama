723d3d691850abae9e06a217703da93dfb5beeac
s-nakayama <s-nakayama@beingcorp.co.jp>
2026-04-09 11:30:09 +0900

    #612414【アプリ開発③】データクラスの実装
    別システムから出力されるCSVデータを読み込み、売上を集計するためのデータクラスを実装しました。
    実装内容は以下の通りです。
    ・店舗、商品、在庫、売上のCSVデータを受け取るクラスを作成。
    ・集計結果、発注要否に用いるクラスを作成。

---
+++ SalesManager/SalesManager/Models/InventoryModel.cs
@@ -0,0 +1,21 @@
+﻿namespace SalesManager.Models {
+    /// <summary>
+    /// 在庫データを保持するクラス
+    /// </summary>
+    public class InventoryModel {
+        /// <summary>
+        /// 店舗ID
+        /// </summary>
+        public int FStoreId { get; set; }
+
+        /// <summary>
+        /// 商品ID
+        /// </summary>
+        public int FProductId { get; set; }
+
+        /// <summary>
+        /// 在庫数量
+        /// </summary>
+        public int FStock { get; set; }
+    }
+}
---
+++ SalesManager/SalesManager/Models/ProductModel.cs
@@ -0,0 +1,26 @@
+﻿namespace SalesManager.Models {
+    /// <summary>
+    /// 商品マスタデータを保持するクラス
+    /// </summary>
+    public class ProductModel {
+        /// <summary>
+        /// 商品ID
+        /// </summary>
+        public int FProductId { get; set; }
+
+        /// <summary>
+        /// 商品名
+        /// </summary>
+        public string FProductName { get; set; }
+
+        /// <summary>
+        /// 単価
+        /// </summary>
+        public decimal FUnitPrice { get; set; }
+
+        /// <summary>
+        /// カテゴリー
+        /// </summary>
+        public string FCategory { get; set; }
+    }
+}
