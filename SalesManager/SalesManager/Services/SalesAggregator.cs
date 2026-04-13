11d16bbae339bc73fd7962f3e2c7b2980f703f03
s-nakayama <s-nakayama@beingcorp.co.jp>
2026-04-13 16:28:59 +0900

    #612896【アプリ開発③】売上集計・締め処理ロジックの実装
    売上データ、店舗、製品、在庫の各マスタを統合し、集計結果を生成するロジックを実装しました。
    実装内容は以下の通りです。
    【SalesAggregator.cs】
    ・集計・結合・変換の3つの責務をメソッド単位で分離。
    ・発注閾値(FRestockThreshold)をフィールド定数として定義。
    ・マスタデータ(店舗、製品、在庫)をIDをキーとしたDictionaryへ変換することで検索を最適化。
    ・LINQを用いて中間集計するロジックを実装。
    ・マスタデータの有無確認と取得を1つのメソッド（TryGetMasterData）に集約。
    ・販売後の在庫数(残数)の算出、および発注要否の判定ロジック (CreateDto)を実装。
    【その他】
    ・Data、Servicesフォルダの.gitkeep（空ファイル）を削除。

---
+++ SalesManager/SalesManager/Services/SalesAggregator.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SalesManager.Models;
+
+namespace SalesManager.Business {
+    /// <summary>
+    /// 各種データを統合し、集計結果を生成するクラス
+    /// </summary>
+    public class SalesAggregator {
+
+        private readonly int FRestockThreshold = 5;
+
+        /// <summary>
+        /// 売上データを各種モデルと結合し、集計結果のDTOリストを生成するメソッド
+        /// </summary>
+        /// <param name="vSales">売上データのリスト</param>
+        /// <param name="vStores">店舗データのリスト</param>
+        /// <param name="vProducts">商品データのリスト</param>
+        /// <param name="vInventories">在庫データのリスト</param>
+        /// <returns>集計結果</returns>
+        public List<AggregatedSalesDto> Aggregate(
+            IReadOnlyList<SaleModel> vSales,
+            IReadOnlyList<StoreModel> vStores,
+            IReadOnlyList<ProductModel> vProducts,
+            IReadOnlyList<InventoryModel> vInventories) {
+
+            var wStoreDict = vStores.ToDictionary(x => x.FStoreId);
+            var wProductDict = vProducts.ToDictionary(x => x.FProductId);
+            var wInventoryDict = vInventories.ToDictionary(x => (x.FStoreId, x.FProductId));
+
+            var wResultList = new List<AggregatedSalesDto>();
+
+            var wGroupedSales = vSales
+                .GroupBy(x => (x.FStoreId, x.FProductId))
+                .Select(y => (
+                    y.Key.FStoreId,
+                    y.Key.FProductId,
+                    y.Sum(x => x.FQuantity)
+                ));
+
+            foreach (var wSale in wGroupedSales) {
+                if (!TryGetMasterData(wSale, wStoreDict, wProductDict, wInventoryDict, out var wLinkedData)) continue;
+
+                wResultList.Add(CreateDto(wSale, wLinkedData));
+            }
+
+            return wResultList;
+        }
+
+        private bool TryGetMasterData(
+            (int FStoreId, int FProductId, int FTotalQuantity) vSale,
+            Dictionary<int, StoreModel> vStoreDict,
+            Dictionary<int, ProductModel> vProductDict,
+            Dictionary<(int, int), InventoryModel> vInventoryDict,
+            out (StoreModel Store, ProductModel Product, InventoryModel Inventory) vData) {
+
+            if (vStoreDict.TryGetValue(vSale.FStoreId, out var wStore) &&
+                vProductDict.TryGetValue(vSale.FProductId, out var wProduct) &&
+                vInventoryDict.TryGetValue((vSale.FStoreId, vSale.FProductId), out var wInventory)) {
+
+                vData = (wStore, wProduct, wInventory);
+                return true;
+            }
+
+            vData = default;
+            return false;
+        }
+
+        private AggregatedSalesDto CreateDto(
+            (int FStoreId, int FProductId, int FTotalQuantity) vSale,
+            (StoreModel Store, ProductModel Product, InventoryModel Inventory) vLinkedData) {
+
+            var wRemaining = vLinkedData.Inventory.FStock - vSale.FTotalQuantity;
+
+            return new AggregatedSalesDto {
+                FStoreName = vLinkedData.Store.FStoreName,
+                FProductName = vLinkedData.Product.FProductName,
+                FTotalSoldQuantity = vSale.FTotalQuantity,
+                FTotalSalesAmount = (decimal)(vSale.FTotalQuantity * vLinkedData.Product.FUnitPrice),
+                FRemainingInventory = wRemaining,
+                FIsRestockNeeded = wRemaining <= FRestockThreshold
+            };
+        }
+    }
+}
