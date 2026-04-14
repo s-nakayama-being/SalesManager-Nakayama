2321c05cf2dbc43d758af5a41519bc77d244c049
s-nakayama <s-nakayama@beingcorp.co.jp>
2026-04-14 17:11:56 +0900

    #612905【アプリ開発③】売上集計・発注候補画面のUI・イベントの実装
    実装前の準備として、フォルダ構成の整備を実施しました。

---
+++ SalesManager/SalesManager/Forms/MainForm.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace SalesManager {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+    }
+}
